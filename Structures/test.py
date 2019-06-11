9ef285981cf5ef4ee97c2aebafcc116f7a27ad49
Stephen Jones <stephenj99642@gmail.com>
2019-06-11 22:17:55 +0100

    Implemented testing for queues

--- Structures/test.py
+++ Structures/test.py
@@ -1,3 +1,48 @@
 """
 Runs tests on the code, accessing it through ctypes.
 """
+
+import ctypes
+
+import pytest
+
+lib = ctypes.cdll.LoadLibrary("build/libCStructures.so")
+
+def test_queues():
+    queue = lib.newQueue(5)
+
+    # Perform the following changes, periodically checking that what comes off is correct
+    assert lib.isQueueEmpty(queue), "Queue should be empty"
+    assert not lib.isQueueFull(queue), "Queue should not be full"
+
+    lib.enqueue(queue, 5)
+    # 5 _ _ _ _
+    lib.enqueue(queue, 6)
+    # 5 6 _ _ _
+    assert lib.dequeue(queue) == 5, "Item dequeued should be 5"
+    # _ 6 _ _ _
+    lib.enqueue(queue, 1)
+    # _ 6 1 _ _
+    lib.enqueue(queue, 2)
+    # _ 6 1 2 _
+    lib.enqueue(queue, 3)
+    # _ 6 1 2 3
+    assert lib.dequeue(queue) == 6, "Item dequeued should be 6"
+    # _ _ 1 2 3
+    lib.enqueue(queue, 4)
+    # 4 _ 1 2 3
+    lib.enqueue(queue, 5)
+    # 4 5 1 2 3
+    assert lib.isQueueFull(queue), "Queue should be full"
+    assert not lib.isQueueEmpty(queue), "Queue should not be empty"
+    assert lib.dequeue(queue) == 1, "Item dequeued should be 1"
+    # 4 5 _ 2 3
+    assert lib.dequeue(queue) == 2, "Item dequeued should be 2"
+    # 4 5 _ _ 3
+    assert lib.dequeue(queue) == 3, "Item dequeued should be 3"
+    # 4 5 _ _ _
+    assert lib.nextInQueue(queue) == 4, "Next item should be 4"
+    assert lib.dequeue(queue) == 4, "Item dequeued should be 4"
+    # _ 5 _ _ _
+    assert lib.dequeue(queue) == 5, "Item dequeued should be 5"
+    assert lib.isQueueEmpty(), "Queue should be empty"
